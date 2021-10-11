6ca37d3c28e22e0676b6e008d9f4892139b062ed
jairoloh <jairolo624@gmail.com>
2021-10-11 15:43:13 -0500

    Delete medical insurance.py

--- medical_insurance.py
+++
@@ -1,8 +0,0 @@
-import csv
-
-with open("insurance.csv") as insurace_file:
-    patients_dict = {}
-    csv_dict = csv.DictReader(insurance_file)
-
-    for key,column in csv_dict:
-        print["name"][5]
